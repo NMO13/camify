0c53a27250e0c3bfb36fb287cfa9d8b1f062a348
martinennemoser <martinennemoser@23c5213d-804b-494e-8a83-dcf9acdd6de9>
2016-08-21 22:03:04 +0000

    Added Winforms theme

--- Camify/Camify/Form1.cs
+++ Camify/Camify/Form1.cs
@@ -7,14 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ComponentFactory.Krypton.Docking;
+using ComponentFactory.Krypton.Navigator;
+using ComponentFactory.Krypton.Toolkit;
 
 namespace Camify
 {
-    public partial class Form1 : Form
+    public partial class Form1 : KryptonForm
     {
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //// Setup docking functionality
+            KryptonDockingWorkspace w = kryptonDockingManager1.ManageWorkspace(kryptonDockableWorkspace1);
+            kryptonDockingManager1.ManageControl(kryptonPanel1, w);
+            kryptonDockingManager1.ManageFloating(this);
+
+            //// Add initial docking pages
+            kryptonDockingManager1.AddToWorkspace("Workspace", new KryptonPage[] { NewDocument(), NewDocument() });
+            kryptonDockingManager1.AddDockspace("Control", DockingEdge.Left, new KryptonPage[] { NewPropertyGrid(), NewInput(), NewPropertyGrid(), NewInput() });
+            kryptonDockingManager1.AddDockspace("Control", DockingEdge.Bottom, new KryptonPage[] { NewInput(), NewPropertyGrid(), NewInput(), NewPropertyGrid() });
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private KryptonPage NewDocument()
+        {
+            KryptonPage page = NewPage("Document ", 0, new Button());
+
+            // Document pages cannot be docked or auto hidden
+            page.ClearFlags(KryptonPageFlags.DockingAllowAutoHidden | KryptonPageFlags.DockingAllowDocked);
+
+            return page;
+        }
+
+        private KryptonPage NewPage(string name, int image, Control content)
+        {
+            // Create new page with title and image
+            KryptonPage p = new KryptonPage();
+            p.Text = name;
+            p.TextTitle = name;
+            p.TextDescription = name;
+           // p.ImageSmall = imageListSmall.Images[image];
+
+            // Add the control for display inside the page
+            content.Dock = DockStyle.Fill;
+            p.Controls.Add(content);
+
+           // _count++;
+            return p;
+        }
+
+        private KryptonPage NewInput()
+        {
+            return NewPage("Input ", 1, new Button());
+        }
+
+        private KryptonPage NewPropertyGrid()
+        {
+            return NewPage("Properties ", 2, new Button());
         }
     }
 }
