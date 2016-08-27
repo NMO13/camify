c67d115d7c18b3b13a25a54595158355732b1316
martinennemoser <martinennemoser@23c5213d-804b-494e-8a83-dcf9acdd6de9>
2016-08-27 10:19:31 +0000

    Missing file added

---
+++ CNCSpecific/Milling/NCProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+using Shared.Geometry;
+
+namespace CNCSpecific.Milling
+{
+    public class NCProgram
+    {
+        private List<NCPath> _paths = new List<NCPath>();
+
+        public void AddPath(Vector3m relativeAmount, int toolId)
+        {
+            var path = new NCPath();
+            path.RelativePosition = relativeAmount;
+            path.ActiveTool = toolId;
+            _paths.Add(path);
+        }
+    }
+}
