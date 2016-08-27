19f1ae9e9ea958eca633216fe7b3b4c792621988
enne87 <enne87@23c5213d-804b-494e-8a83-dcf9acdd6de9>
2016-08-27 09:23:34 +0000

    Renamed Camify to UserInterface

---
+++ RenderEngine/Vertex.cs
@@ -0,0 +1,9 @@
+﻿namespace RenderEngine
+{
+    struct Vertex
+    {
+        public Shared.Geometry.Vector3d Position;
+        public Shared.Geometry.Vector3d Normal;
+        public Shared.Geometry.Vector2d TexCoord;
+    }
+}
