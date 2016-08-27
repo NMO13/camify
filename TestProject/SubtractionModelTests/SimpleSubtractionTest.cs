fa649f70bde8905e8bd3284fbb640f8573099dec
martinennemoser <martinennemoser@23c5213d-804b-494e-8a83-dcf9acdd6de9>
2016-08-27 09:35:46 +0000

    Removed project Camify; Refactoring

---
+++ TestProject/SubtractionModelTests/SimpleSubtractionTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CNCSpecific.Milling;
+using Model;
+using NUnit.Framework;
+using Shared;
+using Shared.Geometry;
+
+namespace TestProject.SubtractionModelTest
+{
+    [TestFixture]
+    class SimpleSubtraction
+    {
+        [SetUp]
+        public void Setup()
+        {
+            MeshModel m = new MeshModel();
+            SubtractionModel s = new SubtractionModel();
+            m.AttachModelObserver(s);
+        }
+
+        [Test]
+        public void Subtraction1()
+        {
+            var program = new NCProgram();
+            program.AddPath(new Vector3m(250, 0, 180));
+            program.AddPath(new Vector3m(0, -150, 0));
+            program.AddPath(new Vector3m(-450, 0, 0));
+            program.AddPath(new Vector3m(0, 0, -400));
+            program.AddPath(new Vector3m(380, 0, 0));
+            program.AddPath(new Vector3m(0, 0, 400));
+            program.AddPath(new Vector3m(-200, 0, -200));
+            program.AddPath(new Vector3m(200, 0, -200));
+            program.AddPath(new Vector3m(-200, 0, 200));
+            program.AddPath(new Vector3m(0, -30, 0));
+            program.AddPath(new Vector3m(0, 60, 0));
+            program.AddPath(new Vector3m(30, 0, 30));
+            program.AddPath(new Vector3m(0, -60, 0));
+            program.AddPath(new Vector3m(0, 60, 0));
+            program.AddPath(new Vector3m(-100, 0, -100));
+            program.AddPath(new Vector3m(0, -60, 0));
+            program.AddPath(new Vector3m(0, 60, 0));
+            program.AddPath(new Vector3m(0, 0, 100));
+            program.AddPath(new Vector3m(0, -60, 0));
+
+        }
+    }
+}
