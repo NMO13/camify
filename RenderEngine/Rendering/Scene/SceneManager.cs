82aeecb87922f98dfcc7c2c1ddb953e4dc89604d
enne87 <enne87@23c5213d-804b-494e-8a83-dcf9acdd6de9>
2016-09-06 09:20:48 +0000

    Added files

---
+++ RenderEngine/Rendering/Scene/SceneManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using GraphicsEngine.Rotation;
+using OpenTK.Graphics.OpenGL;
+using RenderEngine.Camera;
+using RenderEngine.GraphicObjects;
+using RenderEngine.Resources.Shader;
+using Shared.Geometry;
+using Matrix4d = Shared.Geometry.Matrix4d;
+
+namespace RenderEngine.Rendering.Scene
+{
+    class SceneManager
+    {
+        private readonly Renderer _renderer = new Renderer();
+        internal WorldRotator WorldRotator = new WorldRotator();
+
+        internal void Load(int width, int height)
+        {
+            InitializeGl();
+            Resized(width, height);
+            LoadShader();
+            LoadPerpetualObjects();
+        }
+
+        internal void Paint()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            List<IRenderable> renderMeshes = SceneModel.Instance.RenderMeshes;
+            _renderer.Render(renderMeshes);
+        }
+
+        internal void Resized(int width, int height)
+        {
+            if(width > 0 && height > 0)
+            {
+                SceneModel.Instance.SceneWidth = width;
+                SceneModel.Instance.SceneHeight = height;
+
+                double r = (width / (double)height);
+                SceneModel.Instance.ProjectionMatrix = Matrix4d.CreatePerspectiveFieldOfView((float)(0.45 * (1.5 / r)), (float)r,
+                    1f, 2000);
+                GL.Viewport(0, 0, width, height);
+                WorldRotator.SetBounds(width, height);
+            }
+        }
+
+        internal void AdjustCamera()
+        {
+            SceneModel.Instance.LookAtMatrix = Matrix4d.LookAt(Objective.CameraPos, Objective.CameraFront + Objective.CameraPos, Objective.CameraUp); //Camera/View transformation
+            Matrix4d zoom = Matrix4d.CreateTranslation(new Vector3d(0, 0, Objective.CurZoom));
+            SceneModel.Instance.WorldTransformationMatrix = Matrix4d.Mult(SceneModel.Instance.LookAtMatrix, zoom);
+
+            //Rotate camera
+            Matrix4d test = WorldRotator.GetRotationMatrix();
+            Matrix4d rotation = Matrix4d.Mult(Objective.InitialPitch, test);
+            SceneModel.Instance.WorldTransformationMatrix = Matrix4d.Mult(rotation,
+                SceneModel.Instance.WorldTransformationMatrix);
+        }
+
+        private void LoadShader()
+        {
+            ResourceManager.Instance.LoadShader(ShaderLibrary.SceneVertexShader, ShaderLibrary.SceneFragmentShader, null, ShaderLibrary.ShaderName.Scene.ToString());
+            ResourceManager.Instance.LoadShader(ShaderLibrary.MeshVertexShader, ShaderLibrary.MeshFragmentShader, null, ShaderLibrary.ShaderName.Mesh.ToString());
+        }
+
+        private void LoadPerpetualObjects()
+        {
+            RenderObject background = RenderObjectFactory.CreateRenderObject(ObjectType.Background);
+            SceneModel.Instance.AddRenderObject(background);
+        }
+
+        private void InitializeGl()
+        {
+            GL.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+            GL.Viewport(0, 0, SceneModel.Instance.SceneWidth, SceneModel.Instance.SceneHeight);
+        }
+    }
+}
---
+++ RenderEngine/Rendering/Scene/SceneModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using MessageHandling;
+using Model;
+using RenderEngine.Conversion;
+using Shared.Geometry;
+
+namespace RenderEngine.Rendering.Scene
+{
+    public class SceneModel : AbstractModel
+    {
+        public static SceneModel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SceneModel();
+                }
+                return _instance;
+            }
+        }
+
+        internal List<IRenderable> RenderMeshes = new List<IRenderable>();
+        internal int SceneWidth { get; set; }
+        internal int SceneHeight { get; set; }
+        internal bool MeshUpdated { get; set; } //TODO: declare which mesh has been modified! Only mesh that has been changed must be modified.
+
+        //Matrices
+        internal Matrix4d LookAtMatrix { get; set; }
+        internal Matrix4d WorldTransformationMatrix { get; set; }
+        internal Matrix4d ProjectionMatrix { get; set; }
+        internal Matrix4d RotationMatrix { get; set; }
+
+        //Singletong
+        private static SceneModel _instance;
+        private event ModelHandler<AbstractModel> Changed;
+
+        //Constructor
+        private SceneModel() { }
+
+        internal void AddRenderObject(IRenderable renderObject)
+        {
+            RenderMeshes.Add(renderObject);
+        }
+
+        public override void AttachObserver(IObserver observer)
+        {
+            Changed += observer.Notified;
+        }
+
+        public override void AttachModelObserver(AbstractModel abstractModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ModelNotified(AbstractModel sender, Message message)
+        {
+            if (message.MessageType == MessageType.NewRoughParts)
+            {
+                var meshMessage = message as MeshMessage;
+                RenderMeshes.AddRange(Converter.ToRenderMeshes(meshMessage.GetMeshes));
+            }
+        }
+    }
+}
