4c4bf255231e9861dd6e716d46fd0ffeb394d157
martinennemoser <martinennemoser@23c5213d-804b-494e-8a83-dcf9acdd6de9>
2016-09-10 09:43:16 +0000

    Forgotten to commit.

---
+++ MessageHandling/Messages/HeMeshMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphicsEngine.HalfedgeMesh;
+
+namespace MessageHandling.Messages
+{
+    public class HeMeshMessage : Message
+    {
+        public List<HeMesh> Meshes = new List<HeMesh>();
+        public HeMeshMessage(MessageType type, List<HeMesh> meshes) : base(type)
+        {
+            Meshes = meshes;
+        }
+
+        public List<HeMesh> GetMeshes { get { return Meshes; } }
+    }
+}
